ab1b931b62b976b4602f938812633f3cd632d0ed
M1SS1NGHU3S <ruanvictor.qa@gmail.com>
2024-05-28 19:28:01 -0300

    Inicio do Projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/ScreenMatchApplication.java
@@ -0,0 +1,27 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumeApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenMatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenMatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumirApi = new ConsumeApi();
+		var json = consumirApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=bf73ac45&season=1");
+
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+	}
+}
---
+++ src/main/java/br/com/alura/screenmatch/service/ConsumeApi.java
@@ -0,0 +1,25 @@
+package br.com.alura.screenmatch.service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumeApi {
+    public String obterDados(String requestUri) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(requestUri))
+                .build();
+        HttpResponse<String> response = null;
+
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return response.body();
+    }
+}
